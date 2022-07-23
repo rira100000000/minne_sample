84cd2ee170969f8613f9ce29acaae21bd351be22
rira100000000 <101010hayakawa@gmail.com>
2022-07-23 05:39:24 +0000

    css fix3

--- app/javascript/order/my_orders.js
+++ app/javascript/order/my_orders.js
@@ -1,4 +1,3 @@
-console.log('1')
 function GethashID (hashIDName){
     if(hashIDName){
       $('.tab li').find('a').each(function() {
@@ -15,14 +14,12 @@ function GethashID (hashIDName){
   }
 $(document).ready(function() {
   $('.tab a').on('click', function() {
-    console.log('3')
     var idName = $(this).attr('href');
     GethashID (idName);
     return false;
   });
 })
   $(window).on('load', function () {
-      console.log('2')
       $('.tab li:first-of-type').addClass("active");
       $('#make').addClass("is-active");
     var hashName = location.hash;
