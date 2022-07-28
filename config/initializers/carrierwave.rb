9ab89324e4a4805393976d0be1d189ed187f894c
rira100000000 <101010hayakawa@gmail.com>
2022-07-28 03:06:02 +0000

    s3対応fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,9 +8,9 @@ CarrierWave.configure do |config|
   config.fog_directory  = 'S3のバケット名'
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: ENV['AWS_DEFAULT_REGION'],
+    aws_access_key_id: ENV['AWS_ACCESS_KEY'],
+    aws_secret_access_key: ENV['AWS_SECRET_KEY'],
+    region: ENV['AWS_REGION'],
     path_style: true
   }
 end
